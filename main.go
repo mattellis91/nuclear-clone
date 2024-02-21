82a1e4bdfaa5afea2281d413eedc9e130acadd13
Matt Ellis <mathew.ellis2@griffithuni.edu.au>
2024-02-21 16:46:00 +1000

    added boilerplate for rendering tiles

--- main.go
+++ main.go
@@ -2,20 +2,66 @@ package main
 
 import (
 	"log"
+	"fmt"
+	"image"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
-type Game struct{}
+const (
+	screenWidth  = 320
+	screenHeight = 240
+)
+
+const (
+	tileSize = 16
+)
+
+var (
+	tilesImage *ebiten.Image
+)
+
+type Game struct{
+	layers [][]int
+}
 
 func (g *Game) Update() error {
 	return nil
 }
 
-func (g *Game) Draw(screen *ebiten.Image) {
-	ebitenutil.DebugPrint(screen, "Hello, World!")
+func init() {
+	var err error
+	tilesImage, _, err = ebitenutil.NewImageFromFile("assets/basictiles.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	w := tilesImage.Bounds().Dx()
+	tileXCount := w / tileSize
+
+	// Draw each tile with each DrawImage call.
+	// As the source images of all DrawImage calls are always same,
+	// this rendering is done very efficiently.
+	// For more detail, see https://pkg.go.dev/github.com/hajimehoshi/ebiten/v2#Image.DrawImage
+	const xCount = screenWidth / tileSize
+	for _, l := range g.layers {
+		for i, t := range l {
+			op := &ebiten.DrawImageOptions{}
+			op.GeoM.Translate(float64((i%xCount)*tileSize), float64((i/xCount)*tileSize))
+
+			sx := (t % tileXCount) * tileSize
+			sy := (t / tileXCount) * tileSize
+			screen.DrawImage(tilesImage.SubImage(image.Rect(sx, sy, sx+tileSize, sy+tileSize)).(*ebiten.Image), op)
+		}
+	}
+
+	ebitenutil.DebugPrint(screen, fmt.Sprintf("TPS: %0.2f", ebiten.ActualTPS()))
+}
+
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
 	return 320, 240
@@ -24,7 +70,11 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 func main() {
 	ebiten.SetWindowSize(640, 480)
 	ebiten.SetWindowTitle("Hello, World!")
-	if err := ebiten.RunGame(&Game{}); err != nil {
+	if err := ebiten.RunGame(&Game{
+		layers: [][]int{
+			{0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
+		},
+	}); err != nil {
 		log.Fatal(err)
 	}
 }
